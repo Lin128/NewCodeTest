adccad7508d67089f1001bd02fde734c20a3c28e
Lin128 <ghostlin128@gmail.com>
2018-09-11 22:08:17 +0800

    0911

--- Python/PythonTest/PythonTest/PythonTest.py
+++ Python/PythonTest/PythonTest/PythonTest.py
@@ -1,6 +1,30 @@
-sss = TimeoutError
-print(sss)
+#定义数据文件
+people={
+    'A':{
+        'phone':'1234',
+        'age':'20'
+        },
+    'B':{
+        'phone':'3333',
+        'age': '24'
+        },
+    'C':{
+        'phone':'3214',
+        'age': '26'
+        }
+    }
 
-print("second update")
+#定义标签
+labels={
+    'phone':'phone number',
+    'age':'years old'
+    }
 
-print("3 update")
+name = input('Please input your name:')
+
+request = input('1)P:phone number \n2)O:years old\nPlease input:')
+
+if request=='P': key ='phone'
+if request=='O': key ='age'
+
+if name in people:print("{0}'s {1} is {2}". name, labels[key], people[name][key] )
