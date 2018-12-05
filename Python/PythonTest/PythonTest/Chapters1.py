ff932fc47951894b215a312114bc3bbffcba9849
guolinqi <ghostlin128@gmail.com>
2018-12-05 22:23:58 +0800

    更新项目1

---
+++ Python/PythonTest/PythonTest/Chapters1.py
@@ -0,0 +1,16 @@
+###List
+bob = ['Bob Smith', 42, 30000, 'SoftWare']
+sue = ['Sue Jones', 45, 45000, 'HardWare']
+
+#print("Bob's Name:" + bob[0])
+
+#print("Sue's Money:" + str(sue[2]))
+
+
+
+#数据库列表
+people =[bob, sue]
+for person in people:
+    print(person)
+
+print(people[1][0])
